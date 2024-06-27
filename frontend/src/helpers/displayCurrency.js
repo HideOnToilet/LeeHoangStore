47ff8c3be85e7813d692576a39979840aace642e
HideOnToilet <144752017+HideOnToilet@users.noreply.github.com>
2024-06-27 20:08:54 +0700

    Update displayCurrency.js

--- frontend/src/helpers/displayCurrency.js
+++ frontend/src/helpers/displayCurrency.js
@@ -1,7 +1,7 @@
 const displayINRCurrency = (num) => {
-    const formatter = new Intl.NumberFormat('en-IN',{
+    const formatter = new Intl.NumberFormat('vi-VN',{
         style : "currency",
-        currency : 'INR',
+        currency : 'VND',
         minimumFractionDigits : 2
     })
 
